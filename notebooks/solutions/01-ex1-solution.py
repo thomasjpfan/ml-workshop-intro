ec678de9091658b0568123ad968e35598c1dd627
Thomas J. Fan <thomasjpfan@gmail.com>
2020-10-24 16:47:44 -0400

    ENH Update notebooks

--- notebooks/solutions/01-ex1-solution.py
+++ notebooks/solutions/01-ex1-solution.py
@@ -1,6 +1,11 @@
 from sklearn.datasets import load_wine
-
 wine = load_wine(as_frame=True)
 wine_df = wine.frame
+
+print(wine.DESCR)
+
+wine.target.value_counts()
+
+wine.data.shape
 
 sns.jointplot(data=wine_df, x="alcohol", y="hue", height=10, hue='target');
